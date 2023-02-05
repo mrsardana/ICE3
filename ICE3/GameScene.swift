8a3bf512984f75c2d50933a1b74d4934b3fa3cea
Deepak Sardana <deepaksardana03@gmail.com>
2023-02-05 12:47:54 -0500

    preloaded sounds impulse

--- ICE3/GameScene.swift
+++ ICE3/GameScene.swift
@@ -1,5 +1,7 @@
 import SpriteKit
 import GameplayKit
+import AVFoundation
+import UIKit
 
 let screenSize = UIScreen.main.bounds
 var screenWidth: CGFloat?
@@ -49,6 +51,23 @@ class GameScene: SKScene
             let cloud = Cloud()
             clouds.append(cloud)
             addChild(cloud)
+        }
+        // preaload / prewarm impulse sounds
+        do
+        {
+            let sounds: [String] = ["thunder", "yay"]
+            for sound in sounds
+            {
+                let path: String = Bundle.main.path(forResource: sound, ofType: "mp3")!
+                let url:URL = URL(fileURLWithPath: path)
+                let avPlayer: AVAudioPlayer = try AVAudioPlayer(contentsOf: url)
+                avPlayer.prepareToPlay()
+            }
+                                    
+        }
+        catch
+        {
+            
         }
 
     }
